50664888d9d25485bb48984b4368196d75df5265
limmie27 <limmie997@gmail.com>
2025-06-01 14:32:46 +0900

    10 + 20 + 30  == 60, g

--- src/main/java/com/back/Calc.java
+++ src/main/java/com/back/Calc.java
@@ -4,7 +4,11 @@ public class Calc {
     public static int run(String expression) {
         if (expression.contains(" + ")) {
             String[] bits = expression.split(" \\+ ");
-            return Integer.parseInt(bits[0]) + Integer.parseInt(bits[1]);
+            int sum = 0;
+            for (String bit : bits) {
+                sum += Integer.parseInt(bit);
+            }
+            return sum;
         } else if (expression.contains(" - ")) {
             String[] bits = expression.split(" - ");
             return Integer.parseInt(bits[0]) - Integer.parseInt(bits[1]);
