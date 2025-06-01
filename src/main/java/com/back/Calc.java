51e108bfbcca9740d53fea84b48cf9ba3c53d014
limmie27 <limmie997@gmail.com>
2025-06-01 16:49:56 +0900

    10 + -20 + 30  == 20, g

--- src/main/java/com/back/Calc.java
+++ src/main/java/com/back/Calc.java
@@ -2,18 +2,21 @@ package com.back;
 
 public class Calc {
     public static int run(String expression) {
-        if (expression.contains(" + ")) {
-            String[] bits = expression.split(" \\+ ");
-            int sum = 0;
-            for (String bit : bits) {
-                sum += Integer.parseInt(bit);
-            }
-            return sum;
-        } else if (expression.contains(" - ")) {
-            String[] bits = expression.split(" - ");
-            return Integer.parseInt(bits[0]) - Integer.parseInt(bits[1]);
-        }
 
-        return 0;
+            String[] bits = expression.split(" ");
+            int result = Integer.parseInt(bits[0]);
+
+            for (int i = 1; i < bits.length; i += 2) {
+            String operator = bits[i];
+            int number = Integer.parseInt(bits[i + 1]);
+
+                if (operator.equals("+")) {
+                result += number;} else if (operator.equals("-")) {
+                    result -= number;
+                }
+            }
+
+
+        return result;
     }
 }
