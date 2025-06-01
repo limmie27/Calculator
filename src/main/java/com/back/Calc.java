8b8471b7e9615c61b055bdfd8f907aeedad40b7f
limmie27 <limmie997@gmail.com>
2025-06-01 17:42:43 +0900

    10 + 5 * 2  == 20,g

--- src/main/java/com/back/Calc.java
+++ src/main/java/com/back/Calc.java
@@ -3,22 +3,32 @@ package com.back;
 public class Calc {
     public static int run(String expression) {
 
-            String[] bits = expression.split(" ");
-            int result = Integer.parseInt(bits[0]);
+        String[] bits = expression.split(" ");
 
-            for (int i = 1; i < bits.length; i += 2) {
-            String operator = bits[i];
+        int current = Integer.parseInt(bits[0]);
+        int result = 0;
+        String operator = "+";
+
+        for (int i = 1; i < bits.length; i += 2) {
+            String op = bits[i];
             int number = Integer.parseInt(bits[i + 1]);
 
-                if (operator.equals("+")) {
-                result += number;} else if (operator.equals("-")) {
-                    result -= number;
-                } else if (operator.equals("*")) {
-                    result *= number;
-                }
-            }
+            if (op.equals("*")) {
+                current *= number;
+            } else {
+                if (operator.equals("+")) result += current;
+                else if (operator.equals("-")) result -= current;
 
+                operator = op;
+                current = number;
+            }
+        }
+
+        if (operator.equals("+")) result += current;
+        else if (operator.equals("-")) result -= current;
 
         return result;
+        }
     }
-}
+
+
